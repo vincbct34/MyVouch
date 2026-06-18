c40fecc5174f50543a4ff9565a0038708b8f08d7
Vincent Bichat <vincent260705@gmail.com>
2026-06-18 14:10:46 +0200

    fix: update padding on legal pages and encode URL in Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,7 +19,7 @@ export async function Footer() {
               rel="noopener noreferrer"
             >
               <img
-                src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=404factory&button_colour=5F7FFF&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00"
+                src="https://img.buymeacoffee.com/button-api/?text=Buy%20me%20a%20coffee&emoji=%E2%98%95&slug=404factory&button_colour=5F7FFF&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00"
                 alt={m.coffee}
                 height={48}
               />
